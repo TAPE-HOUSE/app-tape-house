07df2f3ebae45683f147f57eef845dc8334e0ffb
TAPE-HOUSE <83817435+TAPE-HOUSE@users.noreply.github.com>
2021-05-28 01:21:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    document.getElementById('deviceready').classList.add('ready');
+}
+
 // MenuMobile
 
 const menuBtn = document.querySelector('.menu-btn');
